0b58be2e894efd5caece2e30760f6e7f833b6240
bayajidalam <bayajidalam2001@gmail.com>
2022-11-08 03:08:32 +0600

    banner fake data added

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import deliveryPic from '../../assets/3686725.jpg'
+
+const Banner = () => {
+  return (
+    <div className='bg-slate-300 w-full h-80 rounded-md mt-10 flex mb-10'>
+      <div className='w-1/2'>
+        <p className='text-5xl text-center mt-12 font-bold'>FASTEST</p>
+        <p className='text-5xl text-center mt-8 font-bold'>DELIVERY IN</p>
+        <p className='text-5xl text-center mt-8 font-bold'>BANGLADESH</p>
+      </div>
+      <div className='w-1/2'>
+          <img className='h-72 mt-4 ml-20 rounded-lg' src={deliveryPic} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
