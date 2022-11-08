df904474c99fb1b6d0354446aeff83e2b6a124a8
bayajidalam <bayajidalam2001@gmail.com>
2022-11-08 20:33:20 +0600

    login page added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import img from '../../assets/Login.jpg'
+import { FaFacebook, FaGoogle } from "react-icons/fa";
 
 const Login = () => {
   return (
@@ -6,18 +9,20 @@ const Login = () => {
 
       <div className="hero-content flex-col lg:flex-row">
         
-        <div className="text-center lg:text-left">
-         
+        <div className="text-center lg:text-left w-1/2">
+          <img className="w-3/4 mx-auto rounded-lg" src={img} alt="" />
         </div>
 
-        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <div className="card-body">
+        <div className="card w-1/2 h-[450px]  shadow-2xl bg-base-100">
+          <div className="card-body w-3/4 mx-auto">
+          <p className="text-center text-3xl">Login</p>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
                 type="text"
+                name='email'
                 placeholder="email"
                 className="input input-bordered"
               />
@@ -27,18 +32,24 @@ const Login = () => {
                 <span className="label-text">Password</span>
               </label>
               <input
-                type="text"
+                type="password"
+                name='password'
                 placeholder="password"
                 className="input input-bordered"
               />
               <label className="label">
-                <a href="/" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
+                  <p>Don't have account?<Link className="text-indigo-400" to='/signup'>Sign Up</Link></p>
               </label>
             </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+            <div className="form-control mt-2">
+              <button type="submit" className="btn btn-primary">Login</button>
+              <div className="text-center ">
+                <p className="text-indigo-400 my-2 text-xl">Or Sign In with</p>
+               <div className="flex justify-center ">
+               <FaGoogle className="text-3xl mr-4"/>
+                <FaFacebook className="text-3xl"/>
+               </div>
+              </div>
             </div>
           </div>
         </div>
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,10 +1,76 @@
 import React from 'react';
-
+import { FaFacebook, FaGoogle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import img from '../../assets/signup.jpg'
 const SignUp = () => {
   return (
-    <div>
-      <h1>Sign Up</h1>
+    <div className="hero  bg-slate-300 my-3 rounded-lg">
+
+    <div className="hero-content flex-col lg:flex-row">
+      
+      <div className="text-center lg:text-left w-1/2">
+        <img className="w-3/4 mx-auto rounded-lg" src={img} alt="" />
+      </div>
+
+      <div className="card w-1/2 h-[550px]  shadow-2xl bg-base-100">
+        <div className="card-body w-3/4 mx-auto">
+        <p className="text-center text-3xl">Sign Up</p>
+
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              name='name'
+              placeholder="Name"
+              className="input input-bordered"
+            />
+          </div>
+  
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              name='email'
+              placeholder="email"
+              className="input input-bordered"
+            />
+          </div>
+
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              name='password'
+              placeholder="password"
+              className="input input-bordered"
+            />
+
+            <label className="label">
+                <p>already have an account?<Link className="text-indigo-400" to='/login'>Sign In</Link></p>
+            </label>
+          </div>
+
+          <div className="form-control mt-2">
+            <button type="submit" className="btn btn-primary">Login</button>
+            <div className="text-center ">
+              <p className="text-indigo-400 text-xl my-2">Or Sign In with</p>
+             <div className="flex justify-center ">
+             <FaGoogle className="text-3xl mr-4"/>
+              <FaFacebook className="text-3xl"/>
+             </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
+  </div>
   );
 };
 
